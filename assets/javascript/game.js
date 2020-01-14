0c63af87e922ab158e34596b2c81c6b2eb0ce566
scruffythejanitor <pemberton5150@gmail.com>
2020-01-14 11:48:08 -0700

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,39 @@
-$(document).ready(function () {
-    var movies = ["Inception", "Star Wars", "Indiana Jones","Jurassic Park","Alien", "Rocky", "Pulp Fiction", "ET", "Titanic", "Die Hard", "Ghostbusters", "Forrest Gump", "Lord Of The Rings", "Breakfast Club", "The Thing", "Avatar", "Monty Python And The Holy Grail"];
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+  
+//variables
+var movies = ["Inception", "Star Wars", "Indiana Jones","Jurassic Park","Alien", "Rocky", "Pulp Fiction", "ET", "Titanic", "Die Hard", "Ghostbusters", "Forrest Gump", "Lord Of The Rings", "Breakfast Club", "The Thing", "Avatar", "Monty Python And The Holy Grail"];
+var guessedLetters =[]
+var guesses = 10
+var wins = 0
+var wrongLetter = false
+var wrongSound = Audio
+var rightSound = Audio
+var wordToGuess = []
 
-    
-});
+//picking movie at random
+pickedMovie = movies[Math.floor(Math.random() * movies.length)]
+console.log(pickedMovie);
+
+//Logs letters and spaces for picked movie
+for (var i = 0; i < pickedMovie.length; i++) {
+    if (pickedMovie[i] === " "){
+        wordToGuess.push(" ")
+    } else {
+        wordToGuess.push("_")
+    }  
+}
+console.log(wordToGuess);
+
+//pressed key event
+document.onkeyup = function (typedLetter) {
+   if (alphabet(typedLetter.key)){
+       newfunction(typedLetter.key.toUpperCase)
+   }
+        
+    }
+  
+  //Check if typed letter is in the alphabet
+function alphabet(checkAlpha) {
+    return /^[A-Za-z]+$/.test(checkAlpha);
+}
+
+
