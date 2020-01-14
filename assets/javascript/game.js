e0aef681c58bd320e80697bf5d439ef404d96f53
scruffythejanitor <pemberton5150@gmail.com>
2020-01-14 16:56:34 -0700

    Updated js to reflect dashes for consol log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,89 @@
   
 //variables
 var movies = ["Inception", "Star Wars", "Indiana Jones","Jurassic Park","Alien", "Rocky", "Pulp Fiction", "ET", "Titanic", "Die Hard", "Ghostbusters", "Forrest Gump", "Lord Of The Rings", "Breakfast Club", "The Thing", "Avatar", "Monty Python And The Holy Grail"];
-var guessedLetters =[]
-var guesses = 10
+var wrongLetters = []
+var guesses = 8
 var wins = 0
-var wrongLetter = false
-var wrongSound = Audio
-var rightSound = Audio
+var wrongSound = Audio//sudo
+var rightSound = Audio//sudo
 var wordToGuess = []
+var GuessNumber
+var foundLetter = []
+var guessedLetters = []
 
-//picking movie at random
-pickedMovie = movies[Math.floor(Math.random() * movies.length)]
-console.log(pickedMovie);
+// //Check if typed letter is in the alphabet
+// function alphabet(checkAlpha) {
+//   return /^[A-Za-z]+$/.test(checkAlpha);
+// }
+// function alphabet(keyup)
+//   {
+//    var letters = /^[A-Za-z]+$/;
+//    if(keyup.value.match(letters))
+//      {
+//       return true;
+//      }
+//    else
+//      {
+//      alert("message");
+//      return false;
+//      }
+//   }
 
-//Logs letters and spaces for picked movie
-for (var i = 0; i < pickedMovie.length; i++) {
-    if (pickedMovie[i] === " "){
-        wordToGuess.push(" ")
-    } else {
-        wordToGuess.push("_")
-    }  
+
+//keystroke Listener
+document.addEventListener('keyup', function () { 
+    
+    guessedLetters.push(event.key)
+    console.log(guessedLetters);
+    checkLetterPressed()
+    
+        
+ });
+
+//starts a new game
+ newGame()
+updateHtml()
+//new game function
+function newGame() {
+
+    //picking movie at random
+    pickedMovie = movies[Math.floor(Math.random() * movies.length)]
+    console.log(pickedMovie);
+
+    //reset guesses and word to be guessed
+    guessedLetters = []
+    wordToGuess = []
+
+    //Logs letters and spaces for picked movie
+    for (var i = 0; i < pickedMovie.length; i++) {
+        if (pickedMovie[i] === " "){
+            wordToGuess.push(" ")
+        } else {
+            wordToGuess.push("_")
+        }  
+    }
+    console.log(wordToGuess);
+    
 }
-console.log(wordToGuess);
-
-//pressed key event
-document.onkeyup = function (typedLetter) {
-   if (alphabet(typedLetter.key)){
-       newfunction(typedLetter.key.toUpperCase)
-   }
+// game
+function checkLetterPressed(){
+    for (var i = 0; i < pickedMovie.length; i++) {
+        if (guessedLetters === pickedMovie[i]) {
+            foundLetter.push("")
+            
+        } else{
+            wrongLetters.push("")
+        }
         
     }
-  
-  //Check if typed letter is in the alphabet
-function alphabet(checkAlpha) {
-    return /^[A-Za-z]+$/.test(checkAlpha);
 }
 
 
+//Publish to Index.html
+function updateHtml() {
+    wrongLetters.push
+    document.getElementById("lettersGuessed").innerText = wrongLetters;
+    document.getElementById('lifeLeft').innerText = guesses;
+    document.getElementById('Wins').innerText = wins;
+    document.getElementById('wordGuess').innerText = wordToGuess;
+}
