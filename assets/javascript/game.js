ffe5165295d9a1fdf424bdb42941601339c749d0
scruffythejanitor <pemberton5150@gmail.com>
2020-01-13 18:31:55 -0700

    added some css and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var movies = ["Inception", "Star Wars", "Indiana Jones","Jurassic Park","Alien", "Rocky", "Pulp Fiction", "ET", "Titanic", "Die Hard", "Ghostbusters", "Forrest Gump", "Lord Of The Rings", "Breakfast Club", "The Thing", "Avatar", "Monty Python And The Holy Grail"];
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+    
+});
