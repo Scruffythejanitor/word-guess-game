da24cb3e8a963e71620aa3bbbff252e110c3ee16
scruffythejanitor <pemberton5150@gmail.com>
2020-01-15 21:26:58 -0700

    added sudo code and updated java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,20 @@ var wins = 0
 var wrongSound = Audio//sudo
 var rightSound = Audio//sudo
 var wordToGuess = []
-var GuessNumber
 var foundLetter = []
 var guessedLetters = []
-
-// //Check if typed letter is in the alphabet
-// function alphabet(checkAlpha) {
-//   return /^[A-Za-z]+$/.test(checkAlpha);
-// }
-// function alphabet(keyup)
-//   {
-//    var letters = /^[A-Za-z]+$/;
-//    if(keyup.value.match(letters))
-//      {
-//       return true;
-//      }
-//    else
-//      {
-//      alert("message");
-//      return false;
-//      }
-//   }
-
+var gameIsRunning = false
 
 //keystroke Listener
 document.addEventListener('keyup', function () { 
-    
+   //starts new game
+    if (!gameIsRunning) {
+        startGame() 
+        gameIsRunning = true
+        return
+    }
+    //make event key toUpperCase
+    //limit event.keys to only alpha a-z
     guessedLetters.push(event.key)
     console.log(guessedLetters);
     checkLetterPressed(event.key)
@@ -41,19 +29,15 @@ document.addEventListener('keyup', function () {
         
  });
 
-//starts a new game
-newGame()
-
-
 //new game function
-function newGame() {
+function startGame() {
 
     //picking movie at random
     pickedMovie = movies[Math.floor(Math.random() * movies.length)]
     console.log(pickedMovie);
 
     //reset guesses and word to be guessed
-    guessedLetters = []
+    
     wordToGuess = []
 
     //Logs letters and spaces for picked movie
@@ -64,31 +48,45 @@ function newGame() {
             wordToGuess.push("_")
         }  
     }
+
     console.log(wordToGuess);
-    updateHtml()
 }
 // game
 function checkLetterPressed(letterPressed){
     for (var i = 0; i < pickedMovie.length; i++) {
         if (letterPressed === pickedMovie[i]) {
-            foundLetter.push(letterPressed)
+            foundLetter.push(letterPressed);
+            //play happy sound
+            //need to split pickedMovie
+            //make foundletter.toUpperCase();
+            //fill in found letter with corrisponding letter in pickedMovie
+            //if all letters are guessed in pickedMovie, User wins
+            //update wins
             return;
         }
-        
+                
     }
     wrongLetters.push(letterPressed)
-    if (wrongLetters.includes) {
-        
+    //play sad sound
+    if (wrongLetters.includes = letterPressed) {
+        //keep already guessed letter from being guessed again
     }
-        
+    //-1 from gueesses
+
+    //if all guesses are used, user loses
+    //end game
+
+       console.log(wrongLetters);
+       console.log(foundLetter);
+       
     }
 
 
 
 //Publish to Index.html
 function updateHtml() {
-    document.getElementById("lettersGuessed").innerHTML = wrongLetters;
+    document.getElementById("lettersGuessed").innerText = wrongLetters;
     document.getElementById('lifeLeft').innerText = guesses;
     document.getElementById('wins').innerText = wins;
-    document.getElementById('wordGuess').innerHTML = " " + wordToGuess.join(" ");
+    document.getElementById('wordGuess').innerText = wordToGuess.join(" ");
 }
